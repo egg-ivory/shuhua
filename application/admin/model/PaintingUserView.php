62d8ca4f56fb28519e7d015c85968381a900a124
ivory <944047261@qq.com>
2018-04-20 14:32:39 +0800

    建立视图模型

---
+++ application/admin/model/PaintingUserView.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: FSZY-001
+ * Date: 2018/4/20
+ * Time: 14:27
+ */
+
+namespace app\admin\model;
+
+
+use app\admin\model\base\BaseModel;
+
+class PaintingUserView extends BaseModel
+{
+
+}
